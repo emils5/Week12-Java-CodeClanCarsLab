d608f3910b1399e8d16b882b26fb8e8bd1396a11
Emil <emil.shiva@gmail.com>
2021-08-31 15:00:54 +0100

    Created Customer class

--- src/main/java/Vehicles/Car.java
+++ src/main/java/Vehicles/Car.java
@@ -1,11 +1,19 @@
 package Vehicles;
 
-public class Car {
+public abstract class Car {
     private int colour;
     private double price;
 
     public Car(int colour, double price) {
         this.colour = colour;
         this.price = price;
+    }
+
+    public int getColour() {
+        return colour;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
--- src/main/java/Vehicles/ElectricCar.java
+++ src/main/java/Vehicles/ElectricCar.java
@@ -1,4 +1,7 @@
 package Vehicles;
 
-public class ElectricCar {
+public class ElectricCar extends Car{
+    public ElectricCar(int colour, double price) {
+        super(colour, price);
+    }
 }
